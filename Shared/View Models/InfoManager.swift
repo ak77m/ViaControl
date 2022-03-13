9bf170da21c8bfc3081e3a6f4f04b9e41178bd1d
No <ak77m@mail.ru>
2022-03-14 00:11:41 +0300

    WOL  bag fixed

--- Shared/View Models/InfoManager.swift
+++ Shared/View Models/InfoManager.swift
@@ -44,8 +44,6 @@ class InfoManager: ObservableObject {
      @Published var loginString : String = "Логин"
      @Published var resultString : String = "Ответ"
      
-    let connectionString  = "<P><UN>su</UN><Pwd>supass</Pwd><Cmd>Login</Cmd><P1></P1><P 2></P2><P3></P3><P4></P4><P5></P5><P6></P6><P7></P7><P8></P8><P9></P9> <P10></P10></P>"
-    
     func login() {
         let loginString = RequestString(login: activeHost.login,
                                         password: activeHost.password,
